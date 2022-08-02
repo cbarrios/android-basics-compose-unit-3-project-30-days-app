89ab4e5b046eaa4c541e798e9f273b207636379b
cbarrios <lalobarrios16@yahoo.com>
2022-08-02 16:36:42 -0400

    master: (3) Decide on your app's theme, text, and images

--- app/src/main/java/com/lalosapps/kotlintips/MainActivity.kt
+++ app/src/main/java/com/lalosapps/kotlintips/MainActivity.kt
@@ -3,13 +3,15 @@ package com.lalosapps.kotlintips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lalosapps.kotlintips.ui.theme.KotlinTipsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinTipsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                KotlinApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun KotlinApp() {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(194.dp),
+        painter = painterResource(id = R.drawable.kotlin),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KotlinTipsTheme {
-        Greeting("Android")
+        KotlinApp()
     }
 }
