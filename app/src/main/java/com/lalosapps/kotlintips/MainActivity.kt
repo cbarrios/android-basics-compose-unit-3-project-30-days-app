b05b9c4b7dbae7dd65294769328eb5356df217ef
cbarrios <lalobarrios16@yahoo.com>
2022-08-02 17:35:35 -0400

    master: (4) Decide on the layout for each tip and app

--- app/src/main/java/com/lalosapps/kotlintips/MainActivity.kt
+++ app/src/main/java/com/lalosapps/kotlintips/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.lalosapps.kotlintips.data.TipRepository
+import com.lalosapps.kotlintips.model.Tip
 import com.lalosapps.kotlintips.ui.theme.KotlinTipsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,14 +38,65 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun KotlinApp() {
-    Image(
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        TipList(tips = TipRepository.tips, Modifier.padding(it))
+    }
+}
+
+@Composable
+fun TopAppBar() {
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(194.dp),
-        painter = painterResource(id = R.drawable.kotlin),
-        contentDescription = null,
-        contentScale = ContentScale.Crop
-    )
+            .padding(12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = stringResource(id = R.string.app_name))
+    }
+}
+
+@Composable
+fun TipItem(tip: Tip, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(text = stringResource(id = tip.dayRes))
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = stringResource(id = tip.titleRes))
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                painter = painterResource(id = tip.imageRes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = stringResource(id = tip.descRes))
+        }
+    }
+}
+
+@Composable
+fun TipList(tips: List<Tip>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(tips) {
+            TipItem(tip = it)
+        }
+    }
 }
 
 @Preview(showBackground = true)
